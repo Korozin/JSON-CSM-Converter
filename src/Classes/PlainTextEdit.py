642492e898944e311b518533fe2d9590206d9eb4
Korozin <90534409+Korozin@users.noreply.github.com>
2023-05-03 12:57:15 -0400

    Create custom TextEdit to support D&D

---
+++ src/Classes/PlainTextEdit.py
@@ -0,0 +1,21 @@
+import json
+from PyQt5 import QtWidgets, QtCore, QtGui
+
+class JSON_Text_Box(QtWidgets.QPlainTextEdit):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setAcceptDrops(True)
+    
+    def dragEnterEvent(self, event):
+        #print(event.mimeData().formats())  # print mime types of data being dragged
+        #print(event.mimeData().urls())     # print URLs in drag event's mime data
+        if event.mimeData().hasUrls():
+            url = event.mimeData().urls()[0]
+            if url.isLocalFile() and url.toString().endswith('.json'):  # check if URL is a local file and ends with .json
+                self.dropEvent(url)
+    
+    def dropEvent(self, url):
+        path = url.toLocalFile()  # get file path as string
+        with open(path, 'r') as f:
+            data = json.load(f)
+            self.insertPlainText(json.dumps(data, indent=8))
